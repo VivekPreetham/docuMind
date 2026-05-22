4ee7e9388969bb45ff095a8543671faf19783b4b
VivekPreetham <vivekpreetham12@gmail.com>
2026-05-22 13:55:37 +0530

    initialize backend server with Express, dotenv, and CORS setup

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const connectDB = require("./config/db")
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+        credentials: true
+    })
+);
+
+app.get("/", (req, res) => {
+    res.send("API Running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`); 
+});
